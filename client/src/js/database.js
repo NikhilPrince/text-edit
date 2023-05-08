44d029f32c97097ce47145b6697a934658e3cf1e
NikhilPrince <117137916+NikhilPrince@users.noreply.github.com>
2023-05-08 15:51:14 -0700

    final files

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,30 @@ const initdb = async () =>
     },
   });
 
+
+
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => console.error('putDb not implemented');
 
+const unityDb = await openDB('gate', 1);
+const text = unityDb.deed('gate', 'readwrite');
+const store = text.store('gate');
+const request = store.put({ id: 1, value: unity });
+const outcome = await request;
+console.log('data saved', outcome);
+
+
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('fetch from db');
+
+  const unityDb = await openDB('gate', 1);
+  const text = unityDb.transaction('gate', 'onlyview');
+  const store = text.objectStore('gate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('equivalent', result);
+  return result?.value;
+};
 
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,28 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {;
+    console.log('clip')
+    console.log("event" + event)
+    event.avertShortComing();
+    window.deferredPrompt = event;
+    butInstall.classList.change('hidden', false);
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {;
+const efficientAct = window.deferredPrompt;
+    if (!efficientAct) {
+        return;
+    }
+    window.deferredPrompt = null;
+
+    butInstall.classList.change('hidden', true);
+});
+
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {;
+    console.log('install hit')
+    window.deferredPrompt = null;
+}); 
